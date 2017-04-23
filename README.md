aa9389b4effd291300911d0b02cf793a89f7ca06
Marko Mitranic <marko.mitranic@catenamedia.com>
2017-04-23 22:15:26 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+kad json fajla ima ali je prazan, onda imas gomilu erora nekih
 
 https://www.instagram.com/pinkosensitivism/media/
 https://www.instagram.com/pinkosensitivism/media/?max_id=1450044192985406421_2966022865
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,41 @@
-// The input SCSS files and the SCSS output path
-var scssInput = [
-	'scss/style.scss',
-	'scss/print.scss'
-	];
-var scssOutput = 'app/css';
+var scssInput = ['scss/style.scss'],
+    scssOutput = 'app/css',
+    jsInput = ['scripts/vendor/jquery-3.1.1.min.js', 'scripts/vendor/**/*.js', 'scripts/domain/**/*.js'],
+    jsOutput = 'app/scripts';
 
 // Start everything up.
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 
 // Watch SASS.
 gulp.task('sass', function() {
-  return gulp
-    .src(scssInput)
-    .pipe(sourcemaps.init())
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
-	  
-    .pipe(gulp.dest(scssOutput))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
+    return gulp
+        .src(scssInput)
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(scssOutput))
 });
 
-// Spin up server and reload.
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'app'
-    },
-  })
-})
+gulp.task('scripts', function() {
+    return gulp.src(jsInput)
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest(jsOutput))
+        .pipe(rename('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(jsOutput));
+});
 
-
-gulp.task('watch', ['browserSync', 'sass'], function (){
-  gulp.watch('scss/**/*.scss', ['sass']); 
-  gulp.watch('app/*.html', browserSync.reload); 
-  gulp.watch('app/js/**/*.js', browserSync.reload); 
+gulp.task('watch', ['sass', 'scripts'], function (){
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('scripts/**/*.js', ['scripts']);
 });
