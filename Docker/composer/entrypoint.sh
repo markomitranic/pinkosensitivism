fe09f2bf830e57e408ac2c6c799a0c45851182f1
Marko Mitranic <marko.mitranic@catenamedia.com>
2018-09-01 10:08:42 +0200

    Trying to fix docker paths for entrypoint

--- Docker/composer/entrypoint.sh
+++ Docker/composer/entrypoint.sh
@@ -5,12 +5,6 @@ set -e
 cd /usr/share/nginx/pinko/
 COMPOSER_ALLOW_SUPERUSER=1 composer install
 
-# Install a new cron
-crontab -l > mycron
-echo "0,15,30,45 * * * * php /usr/share/nginx/pinko/bin/console app:instagram:sync" >> mycron
-crontab mycron
-rm mycron
-
 # Tail is used to keep the container running in development environment.
 echo "[Operation] Composer deployment finished. The container will stay up in dev environment."
 tail -f /dev/null
