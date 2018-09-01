58f55d0ed3205860aefa8766643a18541f202e8e
Marko Mitranic <marko.mitranic@catenamedia.com>
2018-09-01 09:46:04 +0200

    Try to install a new cron automatically

--- Docker/composer/entrypoint.sh
+++ Docker/composer/entrypoint.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
+# Start composer
 cd /usr/share/nginx/rush-b/
 COMPOSER_ALLOW_SUPERUSER=1 composer install
+
+# Install a new cron
+crontab -l > mycron
+echo "0,15,30,45 * * * * php /usr/share/nginx/pinko/bin/console app:instagram:sync" >> mycron
+crontab mycron
+rm mycron
 
 # Tail is used to keep the container running in development environment.
 tail -f /dev/null
