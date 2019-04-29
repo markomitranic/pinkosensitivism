98dba25ea9a64da27bbe2646d109d806e6da277c
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-04-30 00:18:14 +0200

    Split composer deploy scripts into dev and prod

--- Docker/composer/entrypoint_dev.sh
+++ Docker/composer/entrypoint_dev.sh
@@ -4,7 +4,7 @@ set -e
 # Start composer
 cd /usr/share/nginx/pinko/
 COMPOSER_ALLOW_SUPERUSER=1 composer install
-php bin/console doctrine:migrations:migrate
+#php bin/console doctrine:migrations:migrate
 APP_ENV=dev APP_DEBUG=1 php bin/console cache:clear
 
 # Tail is used to keep the container running in development environment.
