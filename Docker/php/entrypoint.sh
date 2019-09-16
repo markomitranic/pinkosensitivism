3499c5dcbecb33a6f85f1f7281e016a5acaed61b
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-09-16 22:59:02 +0200

    Implement cron

--- Docker/php/entrypoint.sh
+++ Docker/php/entrypoint.sh
@@ -3,4 +3,14 @@ set -e
 
 php bin/console doctrine:migrations:migrate
 
+echo 'APP_ENV='$APP_ENV >> /etc/environment
+echo 'APP_DEBUG='$APP_DEBUG >> /etc/environment
+echo 'DATABASE_URL='$DATABASE_URL >> /etc/environment
+echo 'INSTAGRAM_CLIENT_ID='$INSTAGRAM_CLIENT_ID >> /etc/environment
+echo 'INSTAGRAM_CLIENT_SECRET='$INSTAGRAM_CLIENT_SECRET >> /etc/environment
+echo 'INSTAGRAM_REDIRECT_URI='$INSTAGRAM_REDIRECT_URI >> /etc/environment
+echo 'SILLY_PASSWORD_PROTECC='$SILLY_PASSWORD_PROTECC >> /etc/environment
+crontab /etc/cron.d/ingest-cron
+cron
+
 php-fpm -F -R
