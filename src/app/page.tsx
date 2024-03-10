6fcfc49f30fba1eb6e0a0a7ec4b82fc48a2114d9
Marko Mitranic <marko.mitranic@gmail.com>
2024-03-10 14:54:13 +0100

    Allow basic post crud operations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import Turso from "~/lib/turso/Turso";
+import Posts from "~/lib/kysely/posts";
 
 export default async function Home() {
-  const posts = await Turso.Posts.list(0, 50);
+  const posts = await Posts.list(0, 50);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
@@ -11,7 +11,7 @@ export default async function Home() {
           <li className="w-1/3" key={post.id}>
             <Image
               src={post.url}
-              alt={post.inserted_at}
+              alt={post.id.toString()}
               width={400}
               height={400}
             />
--- src/lib/kysely/posts.ts
+++ src/lib/kysely/posts.ts
@@ -1,69 +1,24 @@
 import { kyselyClient } from "./kyselyClient";
 
 export default abstract class Posts {
-  // public static async list(offset: number, limit: number) {
-  //   await db
-  //     .insertInto("pet")
-  //     .values({ name: "Catto", species: "cat", owner_id: id })
-  //     .execute();
+  public static async list(offset: number, limit: number) {
+    return kyselyClient
+      .selectFrom("posts")
+      .select(["id", "instagram_date", "instagram_uuid", "url"])
+      .orderBy("instagram_date desc")
+      .offset(offset)
+      .limit(limit)
+      .execute();
+  }
 
-  //   const person = await db
-  //     .selectFrom("person")
-  //     .innerJoin("pet", "pet.owner_id", "person.id")
-  //     .select(["first_name", "pet.name as pet_name"])
-  //     .where("person.id", "=", id)
-  //     .executeTakeFirst();
+  public static async get(id: number) {
+    return kyselyClient
+      .selectFrom("posts")
+      .select(["id", "instagram_date", "instagram_uuid", "url"])
+      .where("id", "=", id)
+      .executeTakeFirstOrThrow();
+  }
 
-  //   const { rows } = await client.execute({
-  //     sql: `SELECT id, instagram_uuid, url, inserted_at FROM Post ORDER BY inserted_at DESC LIMIT :limit OFFSET :offset`,
-  //     args: { limit, offset },
-  //   });
-
-  //   return rows.map((row) => {
-  //     return z
-  //       .object({
-  //         id: z.string(),
-  //         instagram_uuid: z.string(),
-  //         url: z.string(),
-  //         inserted_at: z.string(),
-  //       })
-  //       .parse({
-  //         id: row[0]?.toString(),
-  //         instagram_uuid: row[1],
-  //         url: row[2],
-  //         inserted_at: row[3],
-  //       });
-  //   });
-  // }
-
-  // public static async get(id: bigint | number | string): Promise<Post> {
-  //   const {
-  //     rows: [result],
-  //   } = await client.execute({
-  //     sql: "SELECT id, instagram_uuid, url, inserted_at FROM Post WHERE id = ?",
-  //     args: [id],
-  //   });
-
-  //   if (!result) throw Error("Post cannot be found.");
-
-  //   return z
-  //     .object({
-  //       id: z.string(),
-  //       instagram_uuid: z.string(),
-  //       url: z.string(),
-  //       inserted_at: z.string(),
-  //     })
-  //     .parse({
-  //       id: result[0]?.toString(),
-  //       instagram_uuid: result[1],
-  //       url: result[2],
-  //       inserted_at: result[3],
-  //     });
-  // }
-
-  /**
-   * Creates a new post from a given changeset.
-   */
   public static async create({
     instagram_uuid,
     instagram_date,
