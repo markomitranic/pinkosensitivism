e861632ca99c04a7ccfc3c88d1e1858a12bd90d5
Marko Mitranic <marko.mitranic@gmail.com>
2025-02-22 20:20:22 +0100

    Add Astro DB integration and migration/playground API routes

--- src/pages/api/play.ts
+++ src/pages/api/play.ts
@@ -1,5 +1,6 @@
 import createHttpError from "http-errors";
-import { BlobStorage } from "~/lib/BlobStorage";
+import { z } from "zod";
+import posts from "~/../database.json";
 import { env } from "~/lib/env";
 import { ApiRoute } from "~/lib/utils/ApiRoute";
 
@@ -12,7 +13,18 @@ import { ApiRoute } from "~/lib/utils/ApiRoute";
  * curl -s http://localhost:3000/api/play/ | json_pp
  * ```
  */
+// eslint-disable-next-line @typescript-eslint/require-await
 export const GET = ApiRoute(async () => {
   if (!env.meta.DEV) throw createHttpError.NotFound();
-  return BlobStorage.listAllFiles();
+
+  const validatedPosts = z
+    .object({
+      id: z.number(),
+      instagram_uuid: z.string(),
+      url: z.string(),
+      inserted_at: z.string(),
+    })
+    .array()
+    .parse(posts);
+  return validatedPosts;
 });
